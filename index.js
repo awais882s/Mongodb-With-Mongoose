7f1f7b7f3320cc5b47a35faceb46d61598135c7a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-22 22:14:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
 const main = async () => {
-    mongoose.connect("mongodb+srv://admin:admin@cluster0.cfg2fvd.mongodb.net/e-comm", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-
-    );
-    // Set strictQuery to false
-    mongoose.set('strictQuery', false);
-    // Set strictQuery to false
+    mongoose.connect("mongodb+srv://admin:admin@cluster0.cfg2fvd.mongodb.net/e-comm");
+    // Define your schema and model
     const ProductSchema = new mongoose.Schema({
-        name: String
+        name: String,
+        price: Number
     });
 
     const ProductModel = mongoose.model("products", ProductSchema);
-    let data = new ProductModel({ name: "m8" });
+    let data = new ProductModel({ name: "m8", price: 1000 });
     let result = await data.save();
     console.log(result);
 }
